9c887b66f73a5b321b5035efe687c8aca5c95bd1
Osamh <osamehabdelhamed@gmail.com>
2019-07-16 15:05:45 -0500

    Initial commit

---
+++ OCRParkingLotSite/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using openalprnet;
+
+namespace OCRParkingLotSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                // file is uploaded
+                file.SaveAs(Server.MapPath("~/App_Data/images/") + (Guid.NewGuid().ToString()) + ".png");
+
+                byte[] arr = new byte[1];
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    arr = ms.GetBuffer();
+                }
+
+
+                var alpr = new AlprNet("us", "C:\\Users\\oabdel2\\Downloads\\openalpr-2.3.0-win-64bit\\openalpr_64\\openalpr.conf", "C:\\Users\\oabdel2\\Downloads\\openalpr-2.3.0-win-64bit\\openalpr_64\\runtime_data");
+                if (!alpr.IsLoaded())
+                {
+                    Console.WriteLine("OpenAlpr failed to load!");
+                    
+                }
+                // Optionally apply pattern matching for a particular region
+                alpr.DefaultRegion = "md";
+
+                
+                var results = alpr.Recognize(arr);
+
+                string test = results.Plates.First().TopNPlates.First().ToString();
+                int a = 1;
+                
+            }
+            // after successfully uploading redirect the user
+            return View("Index");
+        }
+
+    }
+}
---
+++ OCRParkingLotSite/Models/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenALPR_MVC_Project.Models
+{
+    [Table("Cars")]
+    public class Car
+    {
+        //Set automatically when images uploaded
+        [Key]
+        public int Key { get; set; }
+        public string Plate { get; set; }
+        public string Timestamp { get; set; }
+        public int NumberOffense { get; set; }
+        public byte[] FrontPicture { get; set; }
+        public byte[] BackPicture { get; set; }
+        //Must be set manually by user
+        public int GotTicketed { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Car() { } //Default constructor
+
+        //Constructor used when POST call made
+        public Car(string make, string model, string color, string platenumber, string fronturl, string backurl)
+        {
+            //Set properties from API call
+            Make = make;
+            Model = model;
+            Color = color;
+            Timestamp = DateTime.Now.ToString(); //Current time as timestamp
+            Plate = platenumber;
+            GotTicketed = 0; // False
+            //Get image blobs
+            using (WebClient w = new WebClient())
+            {
+                FrontPicture = w.DownloadData(fronturl);
+                BackPicture = w.DownloadData(backurl);
+            }
+        }
+    }
+
+    //Class made so that a list of cars can be returned without massive image data
+    public class CarSansPictures
+    {
+        public int Key { get; set; }
+        public string Plate { get; set; }
+        public string Timestamp { get; set; }
+        public int NumberOffense { get; set; }
+        public int GotTicketed { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public CarSansPictures(Car c)
+        {
+            Key = c.Key;
+            Plate = c.Plate;
+            Timestamp = c.Timestamp;
+            NumberOffense = c.NumberOffense;
+            GotTicketed = c.GotTicketed;
+            Make = c.Make;
+            Model = c.Model;
+            Color = c.Color;
+        }
+    }
+}
